4a1cea67e9d900d8a73d38a6dbcd78508315292b
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-06-16 06:26:41 +0200

    Project complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-document.getElementById('usernameInput').addEventListener('keyup', function(event){
-    let username = event.target.value.toLowerCase()
-    console.log(username)
-})
+document
+  .getElementById('searchInput')
+  .addEventListener('keyup', function (event) {
+    let searchQuery = event.target.value.toLowerCase();
+    console.log(searchQuery);
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+    console.log(allNamesDOMCollection[1].textContent);
+    for( let counter = 0; counter < allNamesDOMCollection.length; counter ++ ){
+        const currentName = allNamesDOMCollection[counter].textContent.toLowerCase()
+        console.log(currentName)
+        if (currentName.includes(searchQuery)) {
+            allNamesDOMCollection[counter].style.display = "block";
+        } else {
+            allNamesDOMCollection[counter].style.display = "none";
+        }
+    }
+    
+  });
